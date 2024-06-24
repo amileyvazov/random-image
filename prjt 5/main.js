5a1d8a5db871cfe09af03a22708b50a02dbb2fd1
amilwhithcode <127213330+amileyvazov@users.noreply.github.com>
2024-06-24 21:27:31 +0400

    random-image
    İ use HTML CSS Javascript tecnologies

---
+++ prjt 5/main.js
@@ -0,0 +1,19 @@
+const imgConEl = document.querySelector(".image-container");
+
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("click", () => {
+    imageNum = 10
+    addNewImg()
+});
+
+function addNewImg() {
+    for (let index = 0; index < imageNum; index ++) {
+        const newImgEl = document.createElement("img");
+        newImgEl.src = `https://picsum.photos/300?random=${
+            Math.floor(Math.random() * 2000
+        )}`;
+        imgConEl.appendChild(newImgEl)
+    }
+
+};
